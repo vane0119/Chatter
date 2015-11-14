1962cb9cef43a09a7196cdd987775a543705ab34
vane0119 <vane0119@umn.edu>
2015-11-14 13:29:17 -0600

    fixing README.md description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "REPLACEDTEXT: write description"
+  :url "http://example.com/REPLACEDTEXT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
